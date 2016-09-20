4d5d69b587497d02a556989a4b7a58b83fe61d7c
Matthew Marshall <marsh189@mail.chapman.edu>
2016-09-20 00:42:56 -0700

    Finished app v2.0   fixed crash if no entries were made.

--- TipCalculator/app/src/main/java/edu/chapman/cpsc356/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/edu/chapman/cpsc356/tipcalculator/MainActivity.java
@@ -3,11 +3,13 @@ package edu.chapman.cpsc356.tipcalculator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -40,7 +42,12 @@ public class MainActivity extends AppCompatActivity {
     {
         if(bill.getVisibility() == View.VISIBLE)
         {
-            GetCalculations(Float.parseFloat(cost.getText().toString()), Integer.parseInt(rating.getText().toString()));
+            if(cost.getText().toString().equals("") || rating.getText().toString().equals("")) {
+                Toast.makeText(this, "PLEASE FILL BOTH FIELDS", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                GetCalculations(Float.parseFloat(cost.getText().toString()), Integer.parseInt(rating.getText().toString()));
+            }
         }
         else
         {
@@ -56,53 +63,51 @@ public class MainActivity extends AppCompatActivity {
 
     public void GetCalculations(float c, int r)
     {
-        if(r >= 1 && r <= 3)
-        {
-            finalCost = (c * 0.1f) + c;
-            finalCost = (float)(Math.round(finalCost * 100.0) / 100.0);
-            tipCost.setText("$" + finalCost);
-            description.setText("1-3: 10%");
-        }
-        else if(r >= 4 && r <= 5)
-        {
-            finalCost = (c * 0.13f) + c;
-            finalCost = (float)(Math.round(finalCost * 100.0) / 100.0);
-            tipCost.setText("$" + finalCost);
-            description.setText("4-5: 13%");
-        }
-        else if(r >= 6 && r <= 7)
-        {
-            finalCost = (c * 0.15f) + c;
-            finalCost = (float)(Math.round(finalCost * 100.0) / 100.0);
-            tipCost.setText("$" + finalCost);
-            description.setText("6-7: 15%");
-        }
-        else if(r >= 8 && r <= 9)
-        {
-            finalCost = (c * 0.2f) + c;
-            finalCost = (float)(Math.round(finalCost * 100.0) / 100.0);
-            tipCost.setText("$" + finalCost);
-            description.setText("8-9: 20%");
-        }
-        else if(r == 10)
-        {
-            finalCost = (c * 0.25f) + c;
-            finalCost = (float)(Math.round(finalCost * 100.0) / 100.0);
-            tipCost.setText("$" + finalCost);
-            description.setText("10: 25%");
+        if(r > 0 && r < 11) {
+            if (r >= 1 && r <= 3) {
+                finalCost = (c * 0.1f) + c;
+                finalCost = (float) (Math.round(finalCost * 100.0) / 100.0);
+                tipCost.setText("$" + finalCost);
+                description.setText("1-3: 10%");
+            }
+            else if (r >= 4 && r <= 5) {
+                finalCost = (c * 0.13f) + c;
+                finalCost = (float) (Math.round(finalCost * 100.0) / 100.0);
+                tipCost.setText("$" + finalCost);
+                description.setText("4-5: 13%");
+            }
+            else if (r >= 6 && r <= 7) {
+                finalCost = (c * 0.15f) + c;
+                finalCost = (float) (Math.round(finalCost * 100.0) / 100.0);
+                tipCost.setText("$" + finalCost);
+                description.setText("6-7: 15%");
+            }
+            else if (r >= 8 && r <= 9) {
+                finalCost = (c * 0.2f) + c;
+                finalCost = (float) (Math.round(finalCost * 100.0) / 100.0);
+                tipCost.setText("$" + finalCost);
+                description.setText("8-9: 20%");
+            }
+            else if (r == 10) {
+                finalCost = (c * 0.25f) + c;
+                finalCost = (float) (Math.round(finalCost * 100.0) / 100.0);
+                tipCost.setText("$" + finalCost);
+                description.setText("10: 25%");
+            }
+
+            bill.setVisibility(View.GONE);
+            cost.setVisibility(View.GONE);
+            service.setVisibility(View.GONE);
+            rateText.setVisibility(View.GONE);
+            rating.setVisibility(View.GONE);
+            description.setVisibility(View.VISIBLE);
+            tipCost.setVisibility(View.VISIBLE);
         }
         else
         {
             finalCost = 0;
             tipCost.setText("");
-            description.setText("Please Enter a Rating between 1 and 10");
+            Toast.makeText(this, "RATING MUST BE BETWEEN 1 AND 10", Toast.LENGTH_SHORT).show();
         }
-        bill.setVisibility(View.GONE);
-        cost.setVisibility(View.GONE);
-        service.setVisibility(View.GONE);
-        rateText.setVisibility(View.GONE);
-        rating.setVisibility(View.GONE);
-        description.setVisibility(View.VISIBLE);
-        tipCost.setVisibility(View.VISIBLE);
     }
 }
